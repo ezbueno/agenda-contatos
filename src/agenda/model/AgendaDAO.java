191ec8768fb4d20f455a313a227a0e318a27e494
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-14 23:29:08 -0300

    CREATE/READ

--- src/agenda/model/AgendaDAO.java
+++ src/agenda/model/AgendaDAO.java
@@ -2,6 +2,10 @@ package agenda.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AgendaDAO {
 	
@@ -12,10 +16,10 @@ public class AgendaDAO {
 		
 	private Connection conectar() {
 		Connection con = null;
-		
 		try {
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, user, password);
+			System.out.println("Conexão efetuada com sucesso! " + con);
 			return con;
 		} catch (Exception e) {
 			System.err.println("Erro de conexão: " + e);
@@ -23,12 +27,46 @@ public class AgendaDAO {
 		}
 	}
 	
-	public void testarConexao() {
+	public void inserirContato(AgendaBean contato) {
+		String create = "INSERT INTO contatos(nome, fone, email) VALUES(?, ?, ?)";
+		
 		try {
 			Connection con = conectar();
-			System.out.println("Conexão efetuada com sucesso: " + con);
+			PreparedStatement statement = con.prepareStatement(create);
+			statement.setString(1, contato.getNome());
+			statement.setString(2, contato.getFone());
+			statement.setString(3, contato.getEmail());
+			statement.executeUpdate();
+			con.close();
 		} catch (Exception e) {
-			System.err.println("Erro de conexão: " + e);
+			System.out.println(e);
 		}
 	}
+	
+	public List<AgendaBean> listarContatos() {
+		List<AgendaBean> contatos = new ArrayList<>();
+		String read = "SELECT * FROM contatos ORDER BY nome";
+		
+		try {
+			Connection con = conectar();
+			PreparedStatement statement = con.prepareStatement(read);
+			ResultSet rs = statement.executeQuery();
+			
+			while(rs.next()) {
+				AgendaBean contato = new AgendaBean();
+				contato.setIdcon(rs.getString("idcon"));
+				contato.setNome(rs.getString("nome"));
+				contato.setFone(rs.getString("fone"));
+				contato.setEmail(rs.getString("email"));
+				
+				contatos.add(contato);
+			}
+			con.close();			
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+		return contatos;
+	}
+	
 }

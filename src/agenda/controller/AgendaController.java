4dc30708d29e25bee448ff05170db7e7209aa514
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-14 23:34:52 -0300

    Implemented new method: novoContato

--- src/agenda/controller/AgendaController.java
+++ src/agenda/controller/AgendaController.java
@@ -1,19 +1,24 @@
 package agenda.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import agenda.model.AgendaBean;
 import agenda.model.AgendaDAO;
 
-@WebServlet(urlPatterns = {"/AgendaController", "/main"})
+@WebServlet(urlPatterns = {"/AgendaController", "/main", "/insert"})
 public class AgendaController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private AgendaDAO agendaDAO = new AgendaDAO();
+	private AgendaBean contato = new AgendaBean();
 
     public AgendaController() {
         super();
@@ -24,11 +29,28 @@ public class AgendaController extends HttpServlet {
 		
 		if(action.equals("/main")) {
 			contatos(request, response);
+		} else if(action.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 	
-	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("agenda.jsp");
+	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		List<AgendaBean> listaContatos = agendaDAO.listarContatos();
+		request.setAttribute("contatos", listaContatos);
+		RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+		rd.forward(request, response);
+	}
+	
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		
+		agendaDAO.inserirContato(contato);
+		
+		response.sendRedirect("main");
 	}
 
 }
